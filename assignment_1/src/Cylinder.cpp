f2635da0b33086721afef77997336119564eff1f
Dave Meier <dave.meier2016@gmail.com>
2018-10-04 13:49:03 +0200

    Fixed cylinder-ray-intersection-bug, improved code readability

--- assignment_1/src/Cylinder.cpp
+++ assignment_1/src/Cylinder.cpp
@@ -25,7 +25,14 @@ intersect(const Ray&  _ray,
           vec3&       _intersection_normal,
           double&     _intersection_t) const
 {
-
+    /** \todo
+        * - compute the first valid intersection `_ray` with the cylinder
+        *   (valid means in front of the viewer: t > 0)
+        * - store intersection point in `_intersection_point`
+        * - store ray parameter in `_intersection_t`
+        * - store normal at _intersection_point in `_intersection_normal`.
+        * - return whether there is an intersection with t > 0
+       */
 
     const vec3 &dir = _ray.direction;
     const vec3   oc = _ray.origin - center;
@@ -36,7 +43,6 @@ intersect(const Ray&  _ray,
                                  dot(oc, oc) - (dot(oc, this->axis)*dot(oc, this->axis)) - (radius*radius), t);
 
     _intersection_t = NO_INTERSECTION;
-    bool inside = false;
 
     // Find the closest valid solution (in front of the viewer)
     for (size_t i = 0; i < nsol; ++i) {
@@ -44,83 +50,35 @@ intersect(const Ray&  _ray,
         if (t[i] > 0) {
             double memory = _intersection_t;
             _intersection_t = std::min(_intersection_t, t[i]);
+
+
+            // If the zylinder is less height than the point, go back to previous _intersection_t
             _intersection_point  = _ray(_intersection_t);
             vec3 pointX = operator+(this->center, operator*(dot(operator-(_intersection_point, this->center), this->axis), this->axis));
+
             if (norm(operator-(this->center, pointX)) > (this->height/2)) {
                 _intersection_t = memory;
-                inside = true;
             }
-
-            /* double memory = _intersection_t;
-             _intersection_t = std::min(_intersection_t, t[i]);
-             _intersection_point  = _ray(_intersection_t);
-
-
-             vec3 pointX = operator+(this->center, operator*(dot(operator-(_intersection_point, this->center), this->axis), this->axis));
-             if (norm(operator-(this->center, pointX)) > (this->height/2)) {
-                 _intersection_t = memory;
-                 _intersection_point  = _ray(_intersection_t);
-                 vec3 pointX = operator+(this->center, operator*(dot(operator-(_intersection_point, this->center), this->axis), this->axis));
-                 _intersection_normal = operator-(_intersection_point, pointX);//(_intersection_point - center) / radius;  // TODO: Falsche normale, es sollte der Abstand zur center-linie, nicht zum center-punkt berechnen
-
-
-             } else {
-                 _intersection_normal = operator-(_intersection_point, pointX);//(_intersection_point - center) / radius;  // TODO: Falsche normale, es sollte der Abstand zur center-linie, nicht zum center-punkt berechnen
-
-             }*/
-
-
-
         }
-
-
-
     }
 
-
-
     if (_intersection_t == NO_INTERSECTION) return false;
+
+    // Calculate _intersection_point and pointX (point on center-axis on same height as _intersection_point)
     _intersection_point  = _ray(_intersection_t);
     vec3 pointX = operator+(this->center, operator*(dot(operator-(_intersection_point, this->center), this->axis), this->axis));
+
+    // Assume intersection is outside the cylinder
     _intersection_normal = operator-(_intersection_point, pointX);
 
+    double a = dot(_ray.direction, _intersection_normal) / (norm(_ray.direction) * norm(_intersection_normal));
 
-
-
-    /*operator<<(std::cout, _ray.direction);
-    std::printf(" #### ");
-    operator<<(std::cout, _intersection_normal);
-    std::printf(" #### ");
-   double winkel = dot(_ray.direction, _intersection_normal) / (norm(_ray.direction)*norm(_intersection_normal));
-
-   std::printf("%d\n\n\n\n", winkel);*/
-
-
-
-
-
-    double dot2 = dot(_ray.direction, _intersection_normal);
-    double len1 = norm(_ray.direction);
-    double len2 = norm(_intersection_normal);
-
-    double a = dot2 / (len1 * len2);
-    std::printf("%d ## %d ## %d ## %d\n", dot2, len1, len2, a);
-
-    /*if(winkel < 0) {
+    if (a > 0) {
         _intersection_normal = operator-(pointX, _intersection_point);//(_intersection_point - center) / radius;  // TODO: Falsche normale, es sollte der Abstand zur center-linie, nicht zum center-punkt berechnen
-        //std::cout << "hier";
-    }*/
+    }
 
-   // _intersection_normal = norm(_intersection_normal);
+    // Normalize _intersection_normal
+   _intersection_normal = normalize(_intersection_normal);
 
-
-    /** \todo
-     * - compute the first valid intersection `_ray` with the cylinder
-     *   (valid means in front of the viewer: t > 0)
-     * - store intersection point in `_intersection_point`
-     * - store ray parameter in `_intersection_t`
-     * - store normal at _intersection_point in `_intersection_normal`.
-     * - return whether there is an intersection with t > 0
-    */
     return true;
 }
